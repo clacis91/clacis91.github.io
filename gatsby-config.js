4f0c0ce0132971bcff660f72a200ff054f331f3f
WOOJIN NAM <wjnam@WOOJINui-MacBookPro.local>
2019-07-03 15:32:50 +0900

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Starter Blog`,
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://clacis91.github.io/`,
     social: {
       twitter: `kylemathews`,
     },
