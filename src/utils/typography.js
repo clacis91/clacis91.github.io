b29e02bf899cb6574b8e7855468198932727d0a0
WOOJIN NAM <wjnam@WOOJINui-MacBookPro.local>
2019-07-22 11:34:05 +0900

    font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,8 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 
+Wordpress2016.headerFontFamily = ['Ubuntu','sans-serif']
+Wordpress2016.bodyFontFamily = ['Ubuntu','sans-serif']
 Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
@@ -9,7 +11,7 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
-delete Wordpress2016.googleFonts
+//delete Wordpress2016.googleFonts
 
 const typography = new Typography(Wordpress2016)
 
