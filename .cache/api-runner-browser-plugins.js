4fc8af1929b35873477121e0566e243d89d70b05
wjnam <clacis91@gmail.com>
2022-08-02 00:02:17 +0900

    change directory

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('/Users/wjnam/workspace/clacis91.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":590},
+    },{
+      plugin: require('/Users/wjnam/workspace/clacis91.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/wjnam/workspace/clacis91.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/wjnam/workspace/clacis91.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/wjnam/workspace/clacis91.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
